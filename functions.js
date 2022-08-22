e7ce44ef4e802c5bc6974425c826b511342b391d
likhonalmamun <alikhon055@gmail.com>
2022-08-22 15:59:18 +0600

    added  footer

--- functions.js
+++ functions.js
@@ -18,3 +18,57 @@ function clearInputs() {
   document.getElementById("manager").value = "";
   document.getElementById("coach").value = "";
 }
+
+
+{/* <div
+class="card w-[fit-content] h-[350px] text-center rounded-md bg-black border-2 border-[#434141]"
+>
+  <img class="w-[250px] rounded-sm" src="images/player7.png" alt="" />
+  <h2 class="text-white text-2xl my-3 font-semibold">
+    Cristiano Ronaldo
+  </h2>
+  <p class="text-[#626262] text-md">Click select button to Choose</p>
+  <button
+    class="text-white btn-select bg-blue-800 hover:bg-blue-700 disabled:bg-[#333333] px-[65px] text-md py-2 my-5"
+  >
+    SELECT
+  </button>
+</div>
+<div
+  class="card w-[fit-content] h-[350px] text-center rounded-md bg-black border-2 border-[#434141]"
+>
+  <img class="w-[250px] rounded-sm" src="images/player8.png" alt="" />
+  <h2 class="text-white text-2xl my-3 font-semibold">Pelé</h2>
+  <p class="text-[#626262] text-md">Click select button to Choose</p>
+  <button
+    class="text-white btn-select bg-blue-800 hover:bg-blue-700 disabled:bg-[#333333] px-[65px] text-md py-2 my-5"
+  >
+    SELECT
+  </button>
+</div><div
+          class="card w-[fit-content] h-[350px] text-center rounded-md bg-black border-2 border-[#434141]"
+        >
+          <img class="w-[250px] rounded-sm" src="images/player7.png" alt="" />
+          <h2 class="text-white text-2xl my-3 font-semibold">
+            Cristiano Ronaldo
+          </h2>
+          <p class="text-[#626262] text-md">Click select button to Choose</p>
+          <button
+            class="text-white btn-select bg-blue-800 hover:bg-blue-700 disabled:bg-[#333333] px-[65px] text-md py-2 my-5"
+          >
+            SELECT
+          </button>
+        </div>
+        <div
+          class="card w-[fit-content] h-[350px] text-center rounded-md bg-black border-2 border-[#434141]"
+        >
+          <img class="w-[250px] rounded-sm" src="images/player8.png" alt="" />
+          <h2 class="text-white text-2xl my-3 font-semibold">Pelé</h2>
+          <p class="text-[#626262] text-md">Click select button to Choose</p>
+          <button
+            class="text-white btn-select bg-blue-800 hover:bg-blue-700 disabled:bg-[#333333] px-[65px] text-md py-2 my-5"
+          >
+            SELECT
+          </button>
+        </div>  */}
+       
